8090e2724fa96597408d0006ed3c1db568c05c41
Mathew Gitata <gitataIT@gmail.com>
2019-08-23 00:37:16 +0300

    Validate day entries for the user to avoid entry of unexisting days

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 function fetchAkanName(){
-  var _day = parseInt(document.getElementById("day").value);
-  var _month = parseInt(document.getElementById("month").value);
-  var _year = parseInt(document.getElementById("year").value);
+  var _day = Number(document.getElementById("day").value);
+  var _month = Number(document.getElementById("month").value);
+  var _year = Number(document.getElementById("year").value);
 
   var _male = document.getElementById("male");
   var _female = document.getElementById("female");
 
+  // A function to get user's choice of gender
   function fetchGender(){
     if(_male.checked==true){
       _gender = _male.value;
@@ -15,6 +16,17 @@ function fetchAkanName(){
       alert("No gender selected.");
     }
     return _gender;
+  }
+  var _gender = fetchGender();
+
+  //A function to ensure only correct days entered.
+  function validateDayEntry(){
+    if(_day < 1 || _day >31){
+      alert("Key in an appropriate date");
+      return false;
+    } else {
+      return true;
+    }
   }
 
 }
